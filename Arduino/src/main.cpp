f1ceb9fd1d6ee8cca5df204108f5a2aa05ce487b
daddinuz <daddinuz@gmail.com>
2018-02-27 23:36:47 +0100

    wip on arduino

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 #include <TimerOne.h>
 
 boolean firstRun = true; // Used for one-run-only stuffs;
@@ -8,28 +9,30 @@ const byte PIN_MAX = 17;
 #define RESOLUTION 40 //Microsecond resolution for notes
 
 
-/*NOTE: Many of the arrays below contain unused indexes.  This is 
- to prevent the Arduino from having to convert a pin input to an alternate
- array index and save as many cycles as possible.  In other words information 
- for pin 2 will be stored in index 2, and information for pin 4 will be 
- stored in index 4.*/
+/*NOTE: Many of the arrays below contain unused indexes.  This is
+to prevent the Arduino from having to convert a pin input to an alternate
+array index and save as many cycles as possible.  In other words information
+for pin 2 will be stored in index 2, and information for pin 4 will be
+stored in index 4.*/
 
 
 /*An array of maximum track positions for each step-control pin.  Even pins
- are used for control, so only even numbers need a value here.  3.5" Floppies have
- 80 tracks, 5.25" have 50.  These should be doubled, because each tick is now
- half a position (use 158 and 98).
- */
+are used for control, so only even numbers need a value here.  3.5" Floppies have
+80 tracks, 5.25" have 50.  These should be doubled, because each tick is now
+half a position (use 158 and 98).
+*/
 byte MAX_POSITION[] = {
-  0,0,158,0,158,0,158,0,158,0,158,0,158,0,158,0,158,0};
+  0,0,158,0,158,0,158,0,158,0,158,0,158,0,158,0,158,0
+};
 
 //Array to track the current position of each floppy head.  (Only even indexes (i.e. 2,4,6...) are used)
 byte currentPosition[] = {
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+};
 
 /*Array to keep track of state of each pin.  Even indexes track the control-pins for toggle purposes.  Odd indexes
- track direction-pins.  LOW = forward, HIGH=reverse
- */
+track direction-pins.  LOW = forward, HIGH=reverse
+*/
 int currentState[] = {
   0,0,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW
 };
@@ -42,80 +45,20 @@ unsigned int currentPeriod[] = {
 
 //Current tick
 unsigned int currentTick[] = {
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 };
 
-
-
-//Setup pins (Even-odd pairs for step control and direction
-void setup(){
-  pinMode(13, OUTPUT);// Pin 13 has an LED connected on most Arduino boards
-  pinMode(2, OUTPUT); // Step control 1
-  pinMode(3, OUTPUT); // Direction 1
-  pinMode(4, OUTPUT); // Step control 2
-  pinMode(5, OUTPUT); // Direction 2
-  pinMode(6, OUTPUT); // Step control 3
-  pinMode(7, OUTPUT); // Direction 3
-  pinMode(8, OUTPUT); // Step control 4
-  pinMode(9, OUTPUT); // Direction 4
-  pinMode(10, OUTPUT); // Step control 5
-  pinMode(11, OUTPUT); // Direction 5
-  pinMode(12, OUTPUT); // Step control 6
-  pinMode(13, OUTPUT); // Direction 6
-  pinMode(14, OUTPUT); // Step control 7
-  pinMode(15, OUTPUT); // Direction 7
-  pinMode(16, OUTPUT); // Step control 8
-  pinMode(17, OUTPUT); // Direction 8
-
-  //With all pins setup, let's do a first run reset
-  resetAll();
-  delay(1000);
-	
-  Timer1.initialize(RESOLUTION); // Set up a timer at the defined resolution
-  Timer1.attachInterrupt(tick); // Attach the tick function
-
-  Serial.begin(9600);
-}
-
-void loop(){
-  //Only read if we have 3 bytes waiting
-  if (Serial.available() > 2){
-    //Watch for special 100-message to act on
-    if (Serial.peek() == 100) {
-      //Clear the peeked 100 byte so we can get the following data packet
-      Serial.read();
-      byte byte2 = Serial.read();
-      
-      //This isn't used for anything right now, always set to 0
-      //byte byte3 = Serial.read();
-      
-      switch(byte2) {
-        case 0: resetAll(); break;
-        case 1: break;  //Connected
-        case 2: break;  //Disconnected
-        case 3: break;  //Sequence starting
-        case 4: break;  //Sequence stopping
-        default: resetAll(); break;
-      }      
-      //Flush any remaining messages.
-      while(Serial.available() > 0) { Serial.read(); }
-    } 
-    else{
-      currentPeriod[Serial.read()] = (Serial.read() << 8) | Serial.read();
-    }
-  }
-}
-
+void tick();
+void togglePin(byte pin, byte direction_pin);
 
 /*
 Called by the timer interrupt at the specified resolution.
- */
-void tick()
-{
-  /* 
-   If there is a period set for control pin 2, count the number of
-   ticks that pass, and toggle the pin if the current period is reached.
-   */
+*/
+void tick() {
+  /*
+  If there is a period set for control pin 2, count the number of
+  ticks that pass, and toggle the pin if the current period is reached.
+  */
   if (currentPeriod[2]>0){
     currentTick[2]++;
     if (currentTick[2] >= currentPeriod[2]){
@@ -181,7 +124,7 @@ void togglePin(byte pin, byte direction_pin) {
   if (currentPosition[pin] >= MAX_POSITION[pin]) {
     currentState[direction_pin] = HIGH;
     digitalWrite(direction_pin,HIGH);
-  } 
+  }
   else if (currentPosition[pin] <= 0) {
     currentState[direction_pin] = LOW;
     digitalWrite(direction_pin,LOW);
@@ -190,7 +133,7 @@ void togglePin(byte pin, byte direction_pin) {
   //Update currentPosition
   if (currentState[direction_pin] == HIGH){
     currentPosition[pin]--;
-  } 
+  }
   else {
     currentPosition[pin]++;
   }
@@ -206,10 +149,10 @@ void togglePin(byte pin, byte direction_pin) {
 //
 
 //Not used now, but good for debugging...
-void blinkLED(){
+void blinkLED() {
   digitalWrite(13, HIGH); // set the LED on
   delay(250);              // wait for a second
-  digitalWrite(13, LOW); 
+  digitalWrite(13, LOW);
 }
 
 //For a given controller pin, runs the read-head all the way back to 0
@@ -217,13 +160,13 @@ void reset(byte pin)
 {
   digitalWrite(pin+1,HIGH); // Go in reverse
   for (byte s=0;s<MAX_POSITION[pin];s+=2){ //Half max because we're stepping directly (no toggle)
-    digitalWrite(pin,HIGH);
-    digitalWrite(pin,LOW);
-    delay(5);
-  }
-  currentPosition[pin] = 0; // We're reset.
-  digitalWrite(pin+1,LOW);
-  currentPosition[pin+1] = 0; // Ready to go forward.
+  digitalWrite(pin,HIGH);
+  digitalWrite(pin,LOW);
+  delay(5);
+}
+currentPosition[pin] = 0; // We're reset.
+digitalWrite(pin+1,LOW);
+currentPosition[pin+1] = 0; // Ready to go forward.
 }
 
 //Resets all the pins
@@ -241,18 +184,77 @@ void resetAll(){
 
   // New all-at-once reset
   for (byte s=0;s<80;s++){ // For max drive's position
-    for (byte p=FIRST_PIN;p<=PIN_MAX;p+=2){
-      digitalWrite(p+1,HIGH); // Go in reverse
-      digitalWrite(p,HIGH);
-      digitalWrite(p,LOW);
-    }
-    delay(5);
-  }
-
   for (byte p=FIRST_PIN;p<=PIN_MAX;p+=2){
-    currentPosition[p] = 0; // We're reset.
-    digitalWrite(p+1,LOW);
-    currentState[p+1] = 0; // Ready to go forward.
+    digitalWrite(p+1,HIGH); // Go in reverse
+    digitalWrite(p,HIGH);
+    digitalWrite(p,LOW);
   }
+  delay(5);
+}
 
+for (byte p=FIRST_PIN;p<=PIN_MAX;p+=2){
+  currentPosition[p] = 0; // We're reset.
+  digitalWrite(p+1,LOW);
+  currentState[p+1] = 0; // Ready to go forward.
+}
+
+}
+
+//Setup pins (Even-odd pairs for step control and direction
+void setup(){
+  pinMode(13, OUTPUT);// Pin 13 has an LED connected on most Arduino boards
+  pinMode(2, OUTPUT); // Step control 1
+  pinMode(3, OUTPUT); // Direction 1
+  pinMode(4, OUTPUT); // Step control 2
+  pinMode(5, OUTPUT); // Direction 2
+  pinMode(6, OUTPUT); // Step control 3
+  pinMode(7, OUTPUT); // Direction 3
+  pinMode(8, OUTPUT); // Step control 4
+  pinMode(9, OUTPUT); // Direction 4
+  pinMode(10, OUTPUT); // Step control 5
+  pinMode(11, OUTPUT); // Direction 5
+  pinMode(12, OUTPUT); // Step control 6
+  pinMode(13, OUTPUT); // Direction 6
+  pinMode(14, OUTPUT); // Step control 7
+  pinMode(15, OUTPUT); // Direction 7
+  pinMode(16, OUTPUT); // Step control 8
+  pinMode(17, OUTPUT); // Direction 8
+
+  //With all pins setup, let's do a first run reset
+  resetAll();
+  delay(1000);
+
+  Timer1.initialize(RESOLUTION); // Set up a timer at the defined resolution
+  Timer1.attachInterrupt(tick); // Attach the tick function
+
+  Serial.begin(9600);
+}
+
+void loop(){
+  //Only read if we have 3 bytes waiting
+  if (Serial.available() > 2){
+    //Watch for special 100-message to act on
+    if (Serial.peek() == 100) {
+      //Clear the peeked 100 byte so we can get the following data packet
+      Serial.read();
+      byte byte2 = Serial.read();
+
+      //This isn't used for anything right now, always set to 0
+      //byte byte3 = Serial.read();
+
+      switch(byte2) {
+        case 0: resetAll(); break;
+        case 1: break;  //Connected
+        case 2: break;  //Disconnected
+        case 3: break;  //Sequence starting
+        case 4: break;  //Sequence stopping
+        default: resetAll(); break;
+      }
+      //Flush any remaining messages.
+      while(Serial.available() > 0) { Serial.read(); }
+    }
+    else{
+      currentPeriod[Serial.read()] = (Serial.read() << 8) | Serial.read();
+    }
+  }
 }
